a0b3ce4ecb291938ba6f846ce0797815a1ca4559
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-12-30 11:42:19 +0530

    :zap: :white_check_mark: :wrench: test cases config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import type { UserConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom", // for browser environment simulation
+    css: true, // Optional: handles CSS imports in tests
+    setupFiles: "./src/tests/setup.tsx",
+  },
+} as UserConfig);
